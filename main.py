393c354e5decf652df703ba7c0a943b79212fdeb
Jccalix <jccalix6@gmail.com>
2025-10-28 23:42:22 -0600

    Refactor transaction loading to display debits and credits in separate tabs

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def load_transactions(file):
         df.columns = [col.strip() for col in df.columns]
         df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
-        st.write(df)
         return df
     except Exception as e:
         st.error(f"Error loading file: {str(e)}")
@@ -27,6 +26,17 @@ def main():
 
     if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+        if df is not None:
+            debits_df = df[df["Debit/Credit"] == "Debit"].copy()
+            credits_df = df[df["Debit/Credit"] == "Credit"].copy()
+
+        tab1, tab2 = st.tabs(["Expenses (Debits)", "Payments (Credits)"])
+        with tab1:
+            st.write(debits_df)
+
+        with tab2:
+            st.write(credits_df)
 
 
 main()
